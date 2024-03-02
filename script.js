bc54a805f997431ba03ac452f6eee7e70ce9bebf
shivaay9858 <metador671@gmail.com>
2024-03-02 17:06:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// -------------------------------- Loccomotive --------------------------------
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true
+});
+
+
